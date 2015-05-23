01a79ebac5397615aeff6b30fb7be2ef157a672f
enanablancaynumeros <yeray.alvarez.romero@gmail.com>
2015-05-23 15:50:30 +0100

    ok

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3',
+    version='0.1.4',
     description=long_description,
     long_description=long_description,
     url='https://github.com/enanablancaynumeros/weather_forecast',
@@ -39,8 +39,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
